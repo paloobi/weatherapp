9a7194c98feaf5fc813bf4d75082e6ec01a7a52e
paloobi <alexandra.polubiec@gmail.com>
2014-08-19 20:55:14 -0700

    Simplified code

--- weather.rb
+++ weather.rb
@@ -4,23 +4,14 @@ require 'yahoo_weatherman'
 def determine_weather(location)
 	client = Weatherman::Client.new
 	weather = client.lookup_by_location(location).condition['text']
-	return weather
+	return weather.downcase
 end
 
 def determine_location(location)
 	client = Weatherman::Client.new
 	location_info = client.lookup_by_location(location).location
 	city = location_info['city'].capitalize
-	state = location_info['region'].upcase
-	country = location_info['country'].capitalize
-	final_location = "#{city}, #{state}, #{country}"
-	return final_location
-end
-
-def determine_temp(location)
-	client = Weatherman::Client.new
-	temp = client.lookup_by_location(location).condition['temp']
-	return temp
+	return city
 end
 
 get '/' do
@@ -31,24 +22,22 @@ post '/weather' do
 	@post = params[:post]["location"]
 	@weather = determine_weather(@post)
 	@location = determine_location(@post)
-	@temp = determine_temp(@post)
-		
-	"#{@weather}"
-	
+
 	case @weather
-	when 'Sunny'
-		erb :sunny
-	when 'Cloudy'
-		erb :cloudy
-	when 'Snowy'
-		erb :snowy
-	when 'Pair'
-		erb :fair
-	when 'Partly Cloudy'
-		erb :partly_cloudy
-	when 'Rainy'
-		erb :rainy
-	else
-		erb :default
+		when 'sunny'
+			erb :weather
+		when 'cloudy'
+			erb :weather
+		when 'snowy'
+			erb :weather
+		when 'fair'
+			erb :weather
+		when 'partly cloudy'
+			@weather = 'cloudy'
+			erb :weather
+		when 'rainy'
+			erb :weather
+		else
+			erb :default
 	end
 end
